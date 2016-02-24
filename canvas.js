5ab9a1ea7561265b18ce6bdf0a311b0b74d1721d
SuperJohnZhang <yzhangch@connect.ust.hk>
2016-02-23 23:27:41 -0600

    import the library

--- canvas.js
+++ canvas.js
@@ -1,6 +1,17 @@
-var main(){
-	
+var VSHADE_SOURCE = 
+	' attribute vec4 a_Position;\n' +
+	' attribute vec4 a_Normal;\n' +
+	'  \n'
 
+var FSHADE_SOURCE = 
+	'	\n'
+
+function main(){
+	var canvas = document.getElementById('webgl');
+	var gl = getWebGlContext(canvas);
+	if (!gl){
+		console.log('Failed to retrieve the webgl context for canvas');
+	}
 
 
 
